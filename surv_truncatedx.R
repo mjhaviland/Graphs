f358b44b4903bc4a2eda3b1c3b4df09542067ce2
mjhaviland <36959641+mjhaviland@users.noreply.github.com>
2018-03-01 11:14:09 -0500

    Update surv_truncatedx.R

--- surv_truncatedx.R
+++ surv_truncatedx.R
@@ -1,10 +1,15 @@
-library(haven)
+#this code can be used for all survival curves. The xlim option here is what makes this important as we wanted the x-axis to start 
+# at 20 weeks gestational age. Nothing happened prior to that point.#
+
+#more information can be found here#
+#https://cran.r-project.org/web/packages/survminer/vignettes/Informative_Survival_Plots.html#
+
 library(survminer)
 
-#import dataset#
-ivf<-ivf_ipd_20180220 <- read_dta("S:/OBGYN Research 2/Fellow Research MFM/Katie Johnson/IVF and IPD/SMFM/Preterm IPD/Analysis/ivf_ipd_20180220.dta")
-
-#establish the basic survival curve for IPD#
+#establish the basic survival curve. #
+#time scale=final_ga_deliv_d#
+#exposure=ivf_d#
+#outcome=ipd_d#
 require(survival)
 ipd <- survfit(Surv(final_ga_deliv_d, ipd_d) ~ ivf_d, data = ivf)
 
@@ -16,65 +21,4 @@ ggsurvplot(ipd,
           xlab='Gestational age at delivery (weeks)',
           ylab="Probability of IPD",
           legend.labs=c('No IVF', "IVF"))
-
-
-#model for pec#
-pec <- survfit(Surv(final_ga_deliv_d, final_pec) ~ ivf_d, data = ivf)
-
-ggsurvplot(pec,
-           data=ivf,
-           censor.shape="",
-           xlim=c(20,45),
-           break.time.by=5,
-           xlab='Gestational age at delivery (weeks)',
-           ylab="Probability of PEC",
-           legend.labs=c('No IVF', "IVF"))
-
-#model for abruption#
-abruption <- survfit(Surv(final_ga_deliv_d, final_abruption) ~ ivf_d, data = ivf)
-
-ggsurvplot(abruption,
-           data=ivf,
-           censor.shape="",
-           xlim=c(20,45),
-           break.time.by=5,
-           xlab='Gestational age at delivery (weeks)',
-           ylab="Probability of abruption",
-           legend.labs=c('No IVF', "IVF"))
-
-#model for sga#
-sga <- survfit(Surv(final_ga_deliv_d, final_sga) ~ ivf_d, data = ivf)
-
-ggsurvplot(sga,
-           data=ivf,
-           censor.shape="",
-           xlim=c(20,45),
-           break.time.by=5,
-           xlab='Gestational age at delivery (weeks)',
-           ylab="Probability of SGA",
-           legend.labs=c('No IVF', "IVF"))
-
-#model for IUGR#
-iugr <- survfit(Surv(final_ga_deliv_d, sak_iugr_d) ~ ivf_d, data = ivf)
-
-ggsurvplot(iugr,
-           data=ivf,
-           censor.shape="",
-           xlim=c(20,45),
-           break.time.by=5,
-           xlab='Gestational age at delivery (weeks)',
-           ylab="Probability of IUGR",
-           legend.labs=c('No IVF', "IVF"))
-
-#model for IPD with IUGR#
-ipd_iugr <- survfit(Surv(final_ga_deliv_d, ipd_iugr_d) ~ ivf_d, data = ivf)
-
-ggsurvplot(ipd_iugr,
-           data=ivf,
-           censor.shape="",
-           xlim=c(20,45),
-           break.time.by=5,
-           xlab='Gestational age at delivery (weeks)',
-           ylab="Probability of IPD with IUGR",
-           legend.labs=c('No IVF', "IVF"))
 
